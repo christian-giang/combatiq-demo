f3341e1cd8f903cdc5a0070956ffa1882b9d1e57
christian-giang <36231566+christian-giang@users.noreply.github.com>
2022-04-21 22:12:26 +0200

    legend updated

--- combatiq_demo.py
+++ combatiq_demo.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle as pickle
+import pickle5 as pickle
 from math import pi
 from datetime import datetime
 
